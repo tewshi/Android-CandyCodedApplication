8c378e6e0d05c014d91d1e1a23109bf0075566d3
Joseph Michael <tjoe4u@gmail.com>
2019-10-24 11:07:28 +0100

    set the intent package to google android maps

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -107,5 +107,6 @@ public class MainActivity extends AppCompatActivity {
     public void createMapIntent(View view) {
         Uri mapUri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, mapUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
     }
 }
