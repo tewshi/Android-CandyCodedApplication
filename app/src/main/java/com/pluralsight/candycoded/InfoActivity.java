922084df7244ba04b58dafe0aa0e1356b7ed3a3a
Joseph Michael <tjoe4u@gmail.com>
2019-10-24 11:42:32 +0100

    set the phone intent data to be the phone number as a parsed Uri

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -38,5 +38,6 @@ public class InfoActivity extends AppCompatActivity {
 
     public void createPhoneIntent(View view) {
         Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        phoneIntent.setData(Uri.parse("tel:0123456789"));
     }
 }
