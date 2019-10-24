ce6c89f5a390dd08ca500ddae5ff1ae473d4e084
Joseph Michael <tjoe4u@gmail.com>
2019-10-24 12:25:09 +0100

    add intent extras and starts the activity

--- app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
@@ -78,5 +78,7 @@ public class DetailActivity extends AppCompatActivity {
     private void createShareIntent() {
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, SHARE_DESCRIPTION + mCandyImageUrl + HASHTAG_CANDYCODED);
+        startActivity(shareIntent);
     }
 }
